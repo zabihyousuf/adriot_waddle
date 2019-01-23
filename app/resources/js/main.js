59e5ae44fce0cf9f7195b73470d32e2968c0b3dc
Zabih Yousuf <zy2qd@virginia.edu>
2019-01-22 20:49:49 -0500

    First commit

---
+++ app/resources/js/main.js
@@ -0,0 +1,8 @@
+
+  // Click on add button
+  document.getElementById("add").addEventListener("click", function() {
+    var value = document.getElementById("item").value;
+    if (value){
+      
+    }
+  });
